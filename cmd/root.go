e8c3cd413bb874afb5d4d428b247a7a929566df9
Daniel Rodriguez <dantech2000@gmail.com>
2023-11-11 17:15:45 -0800

    Remove unused code and update DNS server flag

--- cmd/root.go
+++ cmd/root.go
@@ -50,14 +50,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.godig.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.PersistentFlags().StringVarP(&dnsServer, "server", "s", "1.1.1.1:53", "DNS server to use (IP address or hostname)")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
